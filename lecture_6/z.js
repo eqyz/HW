7b83829f4acc9c57f5004169209977edd16ef8f4
eqyz <eqyz00@gmail.com>
2018-08-30 09:02:43 +0300

    Lecture 6 Correction

--- lecture_6/z.js
+++ lecture_6/z.js
@@ -14,11 +14,11 @@ console.log(text3+' PASSWORD ' + PASSWORD.test(text3));
 var text4 =  prompt('Input MAC address', '');
 var MAC = /^(?:[\da-f]{2}:){5}(?:[\da-f]{2})$/i;
 console.log(text4+' MAC address ' + MAC.test(text4));
-
+*/
 var text5 =  prompt('Input IP address', '');
-var IP = /^(?:2[0-5]{2}|[01]?\d?\d)(?:\.(?:2[0-5]{2}|[01]?\d?\d)){3}$/;
+var IP = /^(?:2(?:5[0-5]|[0-4]\d)|[01]?\d?\d)(?:\.(?:2(?:5[0-5]|[0-4]\d)|[01]?\d?\d)){3}$/;
 console.log(text5+' IP address ' + IP.test(text5));
-
+/*
 var text6 =  prompt('Input GUID', '');
 var GUID = /^(?=\{?(?:[\da-f]{8})-(?:[\da-f]{4}-){3}(?:[\da-f]{12})\}?)(?:.{36}|.{38})$/i;
 console.log(text6+' GUID ' + GUID.test(text6));
@@ -26,7 +26,7 @@ console.log(text6+' GUID ' + GUID.test(text6));
 var text7 =  prompt('Input URL', '');
 var URL = /^(?:https?:\/\/)?(?:[a-z]{2,}\.)?[a-z]+\-?[a-z]+\.[a-z]{2,}(?:(?:#|\/)[\da-z/%_.?=&#-]*)?$/i;
 console.log(text7+' URL ' + URL.test(text7));
-*/
+
 function TemplateEngine (temp,obj){
 	return temp.replace(/\{{2}.+?\}{2}/g, function(text){
 		//console.log(text+' ddd');
@@ -36,3 +36,4 @@ function TemplateEngine (temp,obj){
 }
 
 console.log(TemplateEngine('N {{name}} A {{age}} D {{date}}',{date: 2018, name: 'rik', age: 3}));
+*/
